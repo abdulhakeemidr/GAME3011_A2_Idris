699b4deb5d3cda2929db09611c9ee79ad7e62fbf
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-02-24 17:16:32 -0500

    Setting up UI Canvas

---
+++ Assets/[Scripts]/CombinationController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombinationController : MonoBehaviour
+{
+    GameObject upArrowBttn;
+    GameObject downArrowBttn;
+    GameObject combinationText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
